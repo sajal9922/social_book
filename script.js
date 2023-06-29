57eda5c6bdfeba3221727a72e47bbb73853eec55
Sajal <sajal.moni@gmail.com>
2023-06-29 09:53:16 +0300

    Local storage update for webpage theme.

--- script.js
+++ script.js
@@ -7,4 +7,26 @@ function settingsMenuToggle(){
 darkButtonElement.onclick = function(){
   darkButtonElement.classList.toggle('dark-btn-on');
   document.body.classList.toggle('dark-theme');
+
+  if(localStorage.getItem('theme') == 'light'){
+      localStorage.setItem('theme','dark');
+  }
+  else {
+    localStorage.setItem('theme','light');
+  }
+}
+
+
+if(localStorage.getItem('theme') == 'light'){
+  darkButtonElement.classList.remove('dark-btn-on');
+  document.body.classList.remove('dark-theme');
+
+}
+else if(localStorage.getItem('theme') == 'dark'){
+  darkButtonElement.classList.add('dark-btn-on');
+  document.body.classList.add('dark-theme');
+
+}
+else{
+  localStorage.setItem('theme','light');
 }
